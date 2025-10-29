55f5fad214fad058bd3e961f701602721ac2648a
bmeudy11 <bmeudy@gmail.com>
2025-10-29 08:21:07 -0400

    update pom for googlegenai dep, update banner text to show our project name.  Add placeholder endpoint in AIEndpoints class

--- src/main/java/edu/citadel/api/AIEndpoints.java
+++ src/main/java/edu/citadel/api/AIEndpoints.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
+
 @RestController
 @RequestMapping("/suggest")
 public class AIEndpoints {
