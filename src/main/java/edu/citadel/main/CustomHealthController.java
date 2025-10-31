274069d5acea86ec43a346f9cee58535a1a87308
socek <sophiakozlowski4@gmail.com>
2025-10-31 15:17:19 -0400

    Added both a commented out endpoint called "combined" which contains information on both the health and metrics, as well as a modified "health" endpoint containing the same information. To get the health endpoint working, the base health endpoint name was changed to "healthall" and "server.servlet.context-path=/" was added to application.properties.

--- src/main/java/edu/citadel/main/CustomHealthController.java
+++ src/main/java/edu/citadel/main/CustomHealthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+//reconfigured the "health" endpoint with metrics
 @RestController
 public class CustomHealthController {
 
