83f073ee09b5c7e59d2a5468ac105f30f8611140
TaylorVau <tvaughn1@student.citadel.edu>
2025-11-07 17:50:55 -0500

    Created search service for POIs along a route

--- src/main/java/edu/citadel/api/response/AIPOIsResponse.java
+++ src/main/java/edu/citadel/api/response/AIPOIsResponse.java
@@ -1,6 +1,7 @@
 package edu.citadel.api.response;
 
 import edu.citadel.dal.model.Poi;
+import edu.citadel.dal.model.Suggestion;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,35 @@ import java.util.ArrayList;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AIPOIsResponse {
-    private ArrayList<Poi> pois;
+    private ArrayList<Suggestion> suggestions;
+
+    // Alias for backward compatibility - pois and suggestions are the same
+    public ArrayList<Poi> getPois() {
+        ArrayList<Poi> pois = new ArrayList<>();
+        if (suggestions != null) {
+            for (Suggestion suggestion : suggestions) {
+                Poi poi = new Poi();
+                poi.setName(suggestion.getName());
+                poi.setType(suggestion.getType());
+                poi.setAddress(suggestion.getAddress());
+                poi.setReason(suggestion.getReason());
+                pois.add(poi);
+            }
+        }
+        return pois;
+    }
+
+    public void setPois(ArrayList<Poi> pois) {
+        this.suggestions = new ArrayList<>();
+        if (pois != null) {
+            for (Poi poi : pois) {
+                Suggestion suggestion = new Suggestion();
+                suggestion.setName(poi.getName());
+                suggestion.setType(poi.getType());
+                suggestion.setAddress(poi.getAddress());
+                suggestion.setReason(poi.getReason());
+                this.suggestions.add(suggestion);
+            }
+        }
+    }
 }
