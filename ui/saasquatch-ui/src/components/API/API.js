1e5aec18bdeb7af2aec6ef78980fa6be88459eae
Alex Bailey <87785913+baileyam99@users.noreply.github.com>
2025-11-16 07:36:02 -0500

    Deleted unused example api functions

--- ui/saasquatch-ui/src/components/API/API.js
+++ ui/saasquatch-ui/src/components/API/API.js
@@ -46,40 +46,6 @@ const handleRequest = async (requestFn) => {
     }
 };
 
-
-// EXAMPLE ENDPOINTS (keep for reference)
-export const exampleGetEndpoint = async () => {
-    let returnPayload = [true, null];
-
-    try {
-        const url = `${BASE_URL}/exampleGet`;
-        const response = await axios.get(url, {
-            headers: {},
-        });
-        returnPayload = [true, response.data];
-    } catch (e) {
-        console.error(e);
-        returnPayload = [false, e.response?.data?.error || e.message];
-    } finally {
-        return returnPayload;
-    }
-};
-
-export const examplePostEndpoint = async (fakeData) => {
-    try {
-        const response = await axios.post(
-            `${BASE_URL}/examplePost`,
-            { fakeData },
-            { headers: {} },
-        );
-        return [true, response.data];
-    } catch (e) {
-        console.error(e);
-        return [false, e.response?.data?.error || e.message];
-    }
-};
-
-
 /* ROUTE ENDPOINTS
  * Generate a route with Google Maps
  * @param {Object} routeData
